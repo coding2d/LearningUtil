29415b60ec77fcfd5050956b2dcedce473caa242
coding2d <coding2d@gmail.com>
2018-11-17 19:55:07 +0000

    init commit

---
+++ src/com/coding2d/print/array/PrintArray.java
@@ -0,0 +1,35 @@
+package com.coding2d.print.array;
+
+/**
+ * Utility class to print out one and two dimension array to the console
+ * 
+ * @author Kiss Gergely
+ *
+ */
+public class PrintArray {
+	protected static String tabulator = "\t";
+	/**
+	 * Printing out one dimensional array to the console
+	 * 
+	 * @param array
+	 */
+	public static void printArray(Object[] array) {
+		for (Object o : array)
+			System.out.println(o);
+	}
+
+	/**
+	 * Printing out two dimensional array to the console
+	 * 
+	 * @param array
+	 */
+	public static void printArray(Object[][] array) {
+		for (Object[] oa : array) {
+			for (Object o : oa) {
+				System.out.print(o + tabulator);
+			}
+			System.out.println();
+		}
+	}
+
+}
